dbac23d381a4e0ab9d29bb2a840f65d7bc40976f
Chun Yang <canadachunyang@gmail.com>
2021-01-14 23:33:01 -0500

    revise minor error

--- src/components/UploadImage/UploadForm.js
+++ src/components/UploadImage/UploadForm.js
@@ -69,11 +69,9 @@ export default function MultipleImageUploadComponent(props) {
     return (
         <Fragment>
             <Alert show={alert} variant="success" onClose={() => setAlert(false)} dismissible>
-                <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                <Alert.Heading>Upload Success!</Alert.Heading>
                 <p>
-                Change this and that and try again. Duis mollis, est non commodo
-                luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-                Cras mattis consectetur purus sit amet fermentum.
+                    You are successfully upload the images!
                 </p>
             </Alert>
             <form className="upload-form">
